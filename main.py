7a13024372a9c48a153cd8918feabc41411d2437
HelbYT <kuharmaksim2011@gmail.com>
2023-09-08 21:27:26 +0300

    dz2
    Хотів починати зі слова щож знову, тому почну зразу про головне. Це була дуже тяжка робота. ДУЖЕ ДУЖЕ. Я цю роботи робив 4 дні, це не похоже на 1 дз. Розкажу про код трішки.
    
    !!ЦЕ ЧИТАТИ НА КІНЦІ!!!
    На початку я добавив функцію з минулого мого коду калькулятору, якщо ви напишете 0 та нажмете Enter у вас запитається 3 числа, після цих числ у коді перевіряється яке з числе (число1 чи число2) більше. далі у вас йде цикл який закінчиться або коли число ідеально підійде або коли буде менше а якщо ще раз зробити коло то буде більше. Далі переаірка якщо ваше число вже готове то вам буде писати "Вітаємо, ваше число ідеально підійшло" та напише скільки вам треба монеток та по скільки або якщо ваше число не підійшло то йде перевірка бла бла добавиться ваше менше число якщо вийде число яке ви вказали 3 то вам висвітлиться print("Правильно") та скільки треба монеток, якщо ви більш не везучі то вам висвітлиться print("Ваше число не підійшло") та вас знову запитає закінчити чи продовжити
    
    !!!ТУТ Я СТАВ ФІЛОСОФОМ!!!
    Кожному створювачу коду якщо взагалі не лізе в голову коду, то не робіть, йдіть випийте кави, погуляйте  з собакою, поспіть. Та завтра у вас точно все получиться, подумайте про це коли будете гуляти, в вільну хвилинку, але якщо у вас болить голова та лінь то відкиньте це, подумаєте потім
    
    !!!
    Робіть легші дз будь ласка

---
+++ main.py
@@ -0,0 +1,41 @@
+i = 0
+
+while i != 1:
+    i = int(input("Продовжити/Стартувати - 0, закінчити - 1 : "))
+    if (i == 1):
+        break
+
+    number1 = int(input("Введіть 1 число: "))
+    number2 = int(input("Введіть 2 число: "))
+    number3 = int(input("Введіть 3 число: "))
+
+    d = 0
+    iscorrect = 0
+    helping = 0
+    helping2 = 0
+
+    if number1 > number2:
+        helping = number1
+        helping2 = number2
+    else:
+        helping = number2
+        helping2 = number1
+
+    while d <= number3:  
+        if d >= number3:
+            iscorrect = 1
+        d += helping
+
+
+    if d == number3:
+        print("Вітаємо, ваше число ідеально підійшло")
+        d / helping
+        print("Вам потрібно " + d + "монеток по" + helping)
+    else:
+        if (d != number3):
+            c = d / helping
+            d += helping2
+                print("Правильно")
+                print("Вам потрібно " + c + "монеток по " + helping2 + "та одна по" + helping1)
+            else: 
+                print("Ваше число не підійшло")
